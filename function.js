08ccdddc82187124ca3c29639e6d3adbf9d483ed
Agbeti Favour <agbetifavour1@gmail.com>
2021-03-11 10:51:40 +0100

    variables and function

---
+++ function.js
@@ -0,0 +1,26 @@
+// variable
+var a = 5;
+var b = 4;
+var sum = a + b;
+console.log(sum)
+
+// string
+const quote = "A word is enough for the wise";
+console.log(quote);
+
+// conditional statement
+var a = 10
+var b = 6
+var theNumberIsCorrect
+
+if (a = b) {
+  theNumberIsCorrect = true;
+} else {
+  theNumberIsCorrect = false
+}
+
+console.log("The number is correct:" + theNumberIsCorrect)
+
+// array
+ const colors = ["red", "yellow", "orange", "blue", "green", "cream"];
+ console.log(colors);
