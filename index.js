5bfe52843108264a261930df812775c538ccb497
Agbeti Favour <agbetifavour1@gmail.com>
2021-03-11 09:42:03 +0100

    javaScipt assignment

---
+++ index.js
@@ -0,0 +1,19 @@
+function digit(ten,two,yellow) {
+    if (yellow == "multiply"){
+     return (ten * two);
+     
+     } else if (yellow == "divide") {
+
+         return (ten / two);
+
+     } else if (yellow == "sum") {
+
+         return (ten + two);
+     }
+
+
+    
+}
+console.log(digit(100, 50,"multiply"));
+console.log(digit(100, 50,"divide"));
+console.log(digit(100, 50,"sum"));
