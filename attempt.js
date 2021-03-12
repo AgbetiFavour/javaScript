a96681658a930fbad4d7538766bee62a2d0d6aa3
Agbeti Favour <agbetifavour1@gmail.com>
2021-03-12 09:01:02 +0100

    Assignment on attempt

---
+++ attempt.js
@@ -0,0 +1,16 @@
+function number(forty,four,goat) {
+    if (goat == "sum") {
+        return (forty + four);
+    } else if (goat == "multiply") {
+        return (forty * four)
+    } else if (goat == "divide") {
+        return (forty / four)
+    } else {
+        return (forty - four)
+    } 
+}
+
+console.log(number(100,10,"sum"));
+console.log(number(100,10,"multiply"));
+console.log(number(100,10,"divide"));
+console.log(number(100,10,"subtract"));,m
