3099dc7daee9d1a7902e8f2a7a9e0e89f1a3ec53
Agbeti Favour <agbetifavour1@gmail.com>
2021-03-14 23:11:33 +0100

    Second attempt assignment on function

---
+++ attempt2.js
@@ -0,0 +1,24 @@
+
+function person(name,Favour){
+  if(name === Favour) {
+    return true;
+  }
+  
+}
+
+console.log(person());  
+
+function digit(firstNumber,secondNumber,number){
+  if(number === "sum") {
+    return (firstNumber + secondNumber);
+  }
+  
+}
+
+console.log(digit(10,5, "sum"));
+
+function sayHello(){  
+  return ("Hello Dennis")  
+}
+
+console.log(sayHello());
