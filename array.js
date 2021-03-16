71f27856a291a3dc703a80492b2a90a98cad2905
Agbeti Favour <agbetifavour1@gmail.com>
2021-03-16 09:59:41 +0100

    array assignment

---
+++ array.js
@@ -0,0 +1,41 @@
+// Create 3 realistic arrays with at least 3 other realistic arrays as elements
+
+const phoneBrand = [
+    ["Iphone 11","Iphone 6x","Iphone 7x"],
+
+    ["Samsung Galaxy Xcover 5", "Samsung A20", "Samsung A30"],
+
+    ["Nokia C1 Plus", "Nokia C2", "Nokia 9 PureView"]
+];
+
+const laptopBrand = [
+    ["Dell G3","Dell G5","Dell G7"],
+
+    ["Hp pavilion 15", "Hp spectre x360", "Hp EliteBook 850 "],
+
+    ["Acer Chrome Book", "Acer Aspire 3", "Acer Extensa"]
+];
+
+const carBrand = [
+    ["Toyota Avalon","Toyota Camry","Toyota Corolla"],
+
+    ["Mercedes-Benz Coupes", "Mercedes-Benz SUVs", "Mercedes-Benz Sedans"],
+
+    ["Honda Accord", "Honda Fit", "Honda Civic Hybrid"]
+];
+
+console.log(phoneBrand,laptopBrand, carBrand);
+
+// Create an array with two realistic objects as its elements
+
+const webDevelopment = [
+     {
+       frontendFramework: "react.js"
+    },
+
+    {
+        backendFramework: "php"
+    }
+];
+
+console.log(webDevelopment[0])
